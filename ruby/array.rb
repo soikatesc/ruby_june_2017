bc8f6a57f930c573ff35380bcd9c676d892a31e6
Md Rahaman <soikatesc@gmail.com>
2017-06-06 22:42:33 -0500

    Array assignment

---
+++ ruby/array.rb
@@ -0,0 +1,22 @@
+a = ["Md", "Rahaman", "dojo", "Choi", "John"]
+b = [5, 6, 7, 8, 9, 10,11]
+c = ["dojo", 9]
+
+#returns the first value or 0th index of the array
+# puts a[0]
+# puts a[1]
+
+# x =  a+b-c
+# puts x.to_s
+
+#using .delete
+# a.delete("dojo")
+# puts a
+
+#using .push
+a.push("soikat")
+
+
+# puts b.class
+# puts b.shuffle
+# puts b.shuffle.join("-")
